0645b3b4a79155cdd8d330e04a3fb3129f17a889
Albert Rodríguez <albert.rodriguez@in2.es>
2025-10-03 12:59:11 +0200

    improve

--- src/main/java/es/in2/vcverifier/config/WebSocketConfig.java
+++ src/main/java/es/in2/vcverifier/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package es.in2.vcverifier.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -7,8 +8,11 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final BackendConfig backendConfig;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -20,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // Registrar el endpoint de WebSocket para que los clientes se conecten
         registry.addEndpoint("/qr-socket")
-                .setAllowedOrigins("https://verifier.evidenceledger.eu")
+                .setAllowedOrigins(backendConfig.getUrl())
                 .withSockJS();
     }
     
